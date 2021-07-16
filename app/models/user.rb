bd8aa8bd26b88ace6146542d49aa721b04bf7fba
Matias Bermudez <45242740+MatiasAB@users.noreply.github.com>
2021-07-15 20:40:59 -0700

    Update 3 7/15/21 Trying to figure out error stuff on new registrations - of course, the issue was a one line typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,13 @@ class User < ApplicationRecord
   
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   
+  validates :encrypted_password, presence: true
+  
+  validates :password, presence: true
+  
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :username,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
